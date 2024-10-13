e9fb2adf349f2335b4bfb82f721f60edea74ebcb
CarlaZapata14 <carla.zapata@udea.edu.co>
2024-10-12 23:51:38 -0500

    Create estacionservicio.h
    Se agregó el archivo estacionservicio.h, que define la clase EstacionServicio para representar una estación de servicio. Esta clase permite gestionar el nombre, código, gerente, ubicación, surtidores, islas, tanques, y transacciones. Además, incluye funcionalidades para agregar y activar/desactivar surtidores, simular ventas, obtener precios de combustible y gestionar el historial de transacciones.

---
+++ estacionservicio.h
@@ -0,0 +1,51 @@
+#ifndef ESTACIONSERVICIO_H
+#define ESTACIONSERVICIO_H
+
+#include <iostream>
+#include <string>
+#include "preciocombustible.h"
+#include "maquinasurtidora.h"
+#include "puntosurtidor.h"
+#include "coordenadagps.h"
+#include "tanque.h"
+using namespace std;
+
+class EstacionServicio {
+private:
+    string nombre_;
+    string codigo_;
+    string gerente_;
+    enum Region {NORTE, CENTRO, SUR} region_;
+    CoordenadaGPS coordenadaGPS_;
+    Tanque tanque_;
+    Isla* islas_[6];
+    int numIslas_;
+    PuntoSurtidor puntoSurtidor_;
+    MaquinaSurtidora** maquinasSurtidoras_;
+    int numMaquinasSurtidoras_;
+    Transaccion** historicoTransacciones_;
+    int numTransacciones_;
+
+public:
+    EstacionServicio(string& nombre, string& codigo, string& gerente, Region region, CoordenadaGPS coordenadaGPS);
+    ~EstacionServicio();
+    string getNombre();
+    void setNombre(string& nombre);
+    string getCodigo();
+    void setCodigo(string& codigo);
+    string getGerente();
+    void setGerente(string& gerente);
+    Region getRegion();
+    void setRegion(Region region);
+    CoordenadaGPS getCoordenadaGPS();
+    void setCoordenadaGPS(const CoordenadaGPS& coordenadaGPS);
+    float getPrecioCombustible(enum TipoCombustible tipo, enum Region region, PrecioCombustible& precioCombustible);
+    void agregarMaquinaSurtidora(MaquinaSurtidora* maquinaSurtidora);
+    void activarMaquinaSurtidora(MaquinaSurtidora* maquinaSurtidora);
+    void desactivarMaquinaSurtidora(MaquinaSurtidora* maquinaSurtidora);
+    Transaccion* const* getHistorico() const;
+    void simularVenta(PuntoSurtidor* puntoSurtidor, enum TipoCombustible tipo, float litros);
+    void setCapacidadCompartimiento(int capacidad);
+};
+
+#endif // ESTACIONSERVICIO_H
