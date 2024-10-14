30e55887fa28c340dd7cf85ee71b8bc749bea108
Faith Alvarado <faith.alvarado@udea.edu.co>
2024-10-14 15:44:29 -0500

    Actualización: estacionservicio.h
    Se implementaron algunas funcionalidades para establecer las coordenadas GPS, gestionar los codigo de estaciones de forma automatica y seleccionar aleatoriamente el punto surtidor para proceder con la venta

--- estacionservicio.h
+++ estacionservicio.h
@@ -1,51 +1,59 @@
 #ifndef ESTACIONSERVICIO_H
 #define ESTACIONSERVICIO_H
 
-#include <string>
-#include "preciocombustible.h"
-#include "maquinasurtidora.h"
-#include "puntosurtidor.h"
-#include "coordenadagps.h"
 #include "tanque.h"
+#include "isla.h"
+#include "puntosurtidor.h"
+#include "transaccion.h"
+
+#include<string>
 using namespace std;
 
-class EstacionServicio {
+class EstacionServicio{
 private:
     string nombre_;
+    static unsigned int contadorCodigo;
     string codigo_;
     string gerente_;
-    enum Region {NORTE, CENTRO, SUR} region_;
-    CoordenadaGPS coordenadaGPS_;
-    Tanque tanque_;
-    Isla* islas_[6];
-    int numIslas_;
-    PuntoSurtidor puntoSurtidor_;
-    MaquinaSurtidora** maquinasSurtidoras_;
-    int numMaquinasSurtidoras_;
-    Transaccion** historicoTransacciones_;
-    int numTransacciones_;
-    PrecioCombustible precioCombustible_;
+    string region_;
+    float latitud_;
+    float longitud_;
+    Tanque* tanque_;
+    Isla* islas_;
+    unsigned int numPuntosSurtidores_ = 0;
+    unsigned int maxPuntosSurtidores_;
+    PuntoSurtidor** puntosSurtidores_;
+    Transaccion** transacciones_;
+
+    string setCoordenadasGPS(); //Genera aleatoriamente coordenadas
+
 
 public:
-    EstacionServicio(string& nombre, string& codigo, string& gerente, Region region, CoordenadaGPS coordenadaGPS);
+    EstacionServicio();
+    EstacionServicio(string& nombre, string& gerente, string& region, unsigned int maxPuntosSurtidores);
     ~EstacionServicio();
     string getNombre();
-    void setNombre(string& nombre);
-    string getCodigo();
-    void setCodigo(string& codigo);
+    void setNombre(const string& nombre);
+    string getCodigo() const;
+    void setCodigo(const string& codigo);
     string getGerente();
-    void setGerente(string& gerente);
-    Region getRegion();
-    void setRegion(Region region);
-    CoordenadaGPS getCoordenadaGPS();
-    void setCoordenadaGPS(const CoordenadaGPS& coordenadaGPS);
-    float getPrecioCombustible(enum TipoCombustible tipo);
-    void agregarMaquinaSurtidora(MaquinaSurtidora* maquinaSurtidora);
-    void activarMaquinaSurtidora(MaquinaSurtidora* maquinaSurtidora);
-    void desactivarMaquinaSurtidora(MaquinaSurtidora* maquinaSurtidora);
-    Transaccion* const* getHistorico() const;
-    void simularVenta(PuntoSurtidor* puntoSurtidor, enum TipoCombustible tipo, float litros);
-    void setCapacidadCompartimiento(int capacidad);
+    void setGerente(const string& gerente);
+    string getRegion();
+    void setRegion(const string& region);
+    string getCoordenadas();  //Retorna las coordenadas en formato string
+    void agregarPuntoSurtidor(PuntoSurtidor* puntoSurtidor);
+    void eliminarPuntoSurtidor(const string& codigo);
+    void activarPuntoSurtidor(const string& codigo);
+    void desactivarPuntoSurtidor(const string& codigo);
+    void setCapacidadTanque();
+    void simularVenta();
+    void consultarTransaccionesPuntoSurtidor(const string& codigo);
+    void reportarLitrosVendidos();
+    void calcularVentasTotales();
+    bool verificarFugas();
+
+
+
 };
 
 #endif // ESTACIONSERVICIO_H
