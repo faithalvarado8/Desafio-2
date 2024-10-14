23d6fd576644cd054df64e124fc0a18d31f977f3
CarlaZapata14 <carla.zapata@udea.edu.co>
2024-10-13 20:51:35 -0500

    Uso de enums en lugar de strings
    Se modificaron las funciones getPrecio y setPrecio para que utilicen enums en lugar de strings para representar la región y el tipo de combustible. Esto hace que el código sea más eficiente y fácil de mantener.

--- preciocombustible.cpp
+++ preciocombustible.cpp
@@ -1,5 +1,4 @@
 #include "preciocombustible.h"
-#include <stdexcept>
 
 PrecioCombustible::PrecioCombustible(float precioNorteRegular, float precioNortePremium, float precioNorteEcoExtra,
                                      float precioCentroRegular, float precioCentroPremium, float precioCentroEcoExtra,
@@ -15,36 +14,10 @@ PrecioCombustible::PrecioCombustible(float precioNorteRegular, float precioNorte
     precios_[SUR][ECOEXTRA] = precioSurEcoExtra;
 }
 
-float PrecioCombustible::getPrecio(const string& region, const string& tipoCombustible) {
-    Region reg;
-    TipoCombustible tipo;
-
-    if (region == "NORTE") reg = NORTE;
-    else if (region == "CENTRO") reg = CENTRO;
-    else if (region == "SUR") reg = SUR;
-    else throw invalid_argument("Region no valida");
-
-    if (tipoCombustible == "REGULAR") tipo = REGULAR;
-    else if (tipoCombustible == "PREMIUM") tipo = PREMIUM;
-    else if (tipoCombustible == "ECOEXTRA") tipo = ECOEXTRA;
-    else throw invalid_argument("Tipo de combustible no valido");
-
-    return precios_[reg][tipo];
+float PrecioCombustible::getPrecio(Region region, TipoCombustible tipo) {
+    return precios_[region][tipo];
 }
 
-void PrecioCombustible::setPrecio(const string& region, const string& tipoCombustible, float precio) {
-    Region reg;
-    TipoCombustible tipo;
-
-    if (region == "NORTE") reg = NORTE;
-    else if (region == "CENTRO") reg = CENTRO;
-    else if (region == "SUR") reg = SUR;
-    else throw invalid_argument("Region no valida");
-
-    if (tipoCombustible == "REGULAR") tipo = REGULAR;
-    else if (tipoCombustible == "PREMIUM") tipo = PREMIUM;
-    else if (tipoCombustible == "ECOEXTRA") tipo = ECOEXTRA;
-    else throw invalid_argument("Tipo de combustible no valido");
-
-    precios_[reg][tipo] = precio;
+void PrecioCombustible::setPrecio(Region region, TipoCombustible tipo, float precio) {
+    precios_[region][tipo] = precio;
 }
