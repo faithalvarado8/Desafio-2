d57cbdb69a0dfafe6c79aa5eee2766e1b9cabde9
Faith Alvarado <faith.alvarado@udea.edu.co>
2024-10-15 12:11:36 -0500

    Actualizar metodos: isla.h

--- isla.h
+++ isla.h
@@ -16,10 +16,13 @@ private:
 public:
     Isla(string nombre, unsigned int capacidadMaxima = 2);
     ~Isla();
+
+    PuntoSurtidor* getPuntoSurtidor(unsigned int indice);  // Método para obtener un surtidor por índice
+    unsigned int getNumSurtidores();
+
     string getNombreIsla();
     void listarPuntosSurtidores();
     void agregarPuntoSurtidor(PuntoSurtidor* nuevoSurtidor);  // Método para agregar surtidores
-    unsigned int getNumSurtidores();
 
     // // Método estático temporal que devuelve una cantidad de combustible disponible para pruebas
     static float getCantidadDisponible(const string& tipoCombustible);
