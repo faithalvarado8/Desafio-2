83514cfc1d757944633b5019c739402e42d63117
Faith Alvarado <faith.alvarado@udea.edu.co>
2024-10-15 06:18:55 -0500

    Getter nombre isla: isla.h
    Creacion de un getter para acceder al nombre de la isla.

--- isla.h
+++ isla.h
@@ -9,21 +9,20 @@ using namespace std;
 class Isla {
 private:
     string nombre_;
-    unsigned int numPuntosSurtidores_;
-    unsigned int maxSurtidores_;
-    PuntoSurtidor** puntosSurtidores;
+    unsigned int numPuntosSurtidores;
+    unsigned int maxSurtidores;
+    PuntoSurtidor** puntosSurtidores_;
 
 public:
-    Isla(string nombre, unsigned int capacidadMaxima);
+    Isla(string nombre, unsigned int capacidadMaxima = 2);
     ~Isla();
+    string getNombreIsla();
     void listarPuntosSurtidores();
+    void agregarPuntoSurtidor(PuntoSurtidor* nuevoSurtidor);  // Método para agregar surtidores
     unsigned int getNumSurtidores();
-    // Método estático temporal que devuelve una cantidad de combustible disponible para pruebas
+
+    // // Método estático temporal que devuelve una cantidad de combustible disponible para pruebas
     static float getCantidadDisponible(const string& tipoCombustible);
-
-    void agregarPuntoSurtidor(PuntoSurtidor* nuevoSurtidor);  // Método temporal para agregar surtidores
-
 };
 
 #endif // ISLA_H
-
