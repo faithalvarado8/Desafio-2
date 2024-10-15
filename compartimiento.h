fb639cecb1e248c30f16a684c8313b7c8fce4410
Faith Alvarado <faith.alvarado@udea.edu.co>
2024-10-14 22:51:12 -0500

    Eliminacion setter: compartimiento.h
    Se elimina el setter de capacidad por rebundancia. Quien lo asigna es tanque.

--- compartimiento.h
+++ compartimiento.h
@@ -1,20 +1,17 @@
-#ifndef COMPARTIMIENTO_H
-#define COMPARTIMIENTO_H
+#include "compartimiento.h"
 
-class Compartimiento{
-private:
-    float capacidad_;
-    float cantidadActual_;
+//Constructor
+Compartimiento::Compartimiento(float capacidad): capacidad_(capacidad), cantidadActual_(0.0) {
+}
 
-public:
-    //Constructor
-    Compartimiento(float capacidad);
-    //Getters
-    float getCapacidad() const;
-    float getCantidadActual() const;
+//Getters
+float Compartimiento::getCapacidad() const {
+    return capacidad_;
+}
+float Compartimiento::getCantidadActual() const{
+    return cantidadActual_;
+}
 
-    //Setters
-    void setCantidadActual(float cantidad);
-};
-
-#endif // COMPARTIMIENTO_H
+void Compartimiento::setCantidadActual(float cantidad){
+    cantidadActual_ = cantidad;
+}
