84d301867c19a077c6046a4f0deaa62dac9273d9
CarlaZapata14 <carla.zapata@udea.edu.co>
2024-10-16 23:49:29 -0500

    Update tanque.cpp

--- tanque.cpp
+++ tanque.cpp
@@ -1,7 +1,8 @@
 #include "tanque.h"
 #include "compartimiento.h"
 
-Tanque::Tanque(float capacidadRegular, float capacidadPremium, float capacidadEcoExtra): regular_(capacidadRegular), premium_(capacidadPremium), ecoExtra_(capacidadEcoExtra){}
+Tanque::Tanque(float capacidadRegular, float capacidadPremium, float capacidadEcoExtra): regular_(capacidadRegular), premium_(capacidadPremium), ecoExtra_(capacidadEcoExtra){
+}
 
 Compartimiento& Tanque::getRegular(){
     return regular_;
