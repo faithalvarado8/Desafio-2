4b62b33a52ec03a77de5d5015921aee67c344d6f
CarlaZapata14 <carla.zapata@udea.edu.co>
2024-10-16 23:45:59 -0500

    Update puntosurtidor.h

--- puntosurtidor.h
+++ puntosurtidor.h
@@ -1,34 +1,142 @@
-#ifndef PUNTOSURTIDOR_H
-#define PUNTOSURTIDOR_H
+#include "puntosurtidor.h"
+#include "isla.h"
 
-#include <iostream>
-#include <string>
-#include "transaccion.h"
+unsigned int PuntoSurtidor::contadorCodigo = 100;
 
-using namespace std;
+PuntoSurtidor::PuntoSurtidor(string modelo, bool activado, unsigned int numTransacciones_):
+    modelo_(modelo), activado_(activado), numTransacciones_(numTransacciones_) {
+    transacciones_ = new Transaccion*[10];
 
-class PuntoSurtidor {
-private:
-    bool activado_;
-    static unsigned int contadorCodigo;
-    string codigo_;
-    string modelo_;
-    Transaccion** transacciones_;
-    unsigned int numTransacciones_; //Para establecer el tamaño del arreglo
+    codigo_ = "PS-" + to_string(contadorCodigo++);
+}
 
-public:
-    PuntoSurtidor(string modelo, bool activado, unsigned int numTransacciones_);
-    string getCodigo() const;
-    void setCodigo(const string& codigo);
-    string getModelo();
-    bool setEstado(bool estado);
-    bool getEstado() const;
-    void realizarVenta(float precioCombustible);
-    void registrarVenta(Transaccion* venta);
+string PuntoSurtidor::getCodigo()const{
+    return codigo_;
+}
 
-    void mostrarHistorico();
+string PuntoSurtidor::getModelo(){
+    return modelo_;
+}
 
-    ~PuntoSurtidor();
-};
+bool PuntoSurtidor::setEstado(bool estado) {
+    activado_ = estado; // Cambia el estado del surtidor
+    return activado_; // Retorna el nuevo estado
+}
 
-#endif // PUNTOSURTIDOR_H
+bool PuntoSurtidor::getEstado() const {
+    return activado_; // Retorna el estado actual del surtidor
+}
+
+void PuntoSurtidor::realizarVenta(float precioCombustible) {
+
+    if (activado_) {
+
+        char categoriaCombustible;
+        string tipoCombustible;
+        float cantidadCombustible;
+        char categoriaPago;
+        string formaPago;
+        string docCliente;
+        float monto;
+
+        cout << "Tipo de combustible: REGULAR(1), PREMIUM(2), ECOEXTRA(3): ";
+        cin >> categoriaCombustible;
+        if (categoriaCombustible == '1') {
+            tipoCombustible = "REGULAR";
+        }
+        else if (categoriaCombustible == '2') {
+            tipoCombustible = "PREMIUM";
+        }
+        else if (categoriaCombustible == '3') {
+            tipoCombustible = "ECOEXTRA";
+        } else {
+            cout << "Opcion no valida. Por favor, ingrese 1, 2 o 3." << endl;
+        }
+
+        cout << "Ingrese la cantidad de combustible a expender (litros): ";
+        cin >> cantidadCombustible;
+
+        cout << "Forma de pago: EFECTIVO(1), T.DEBITO(2), T.CREDITO(3): ";
+        cin >> categoriaPago;
+        if (categoriaPago == '1') {
+            formaPago = "EFECTIVO";
+        }
+        else if (categoriaPago == '2') {
+            formaPago = "T.DEBITO";
+        }
+        else if (categoriaPago == '3') {
+            formaPago = "T.CREDITO";
+        } else {
+            cout << "Opcion no valida. Por favor, ingrese 1, 2 o 3." << endl;
+        }
+
+        cout << "Ingrese el documento del cliente: ";
+        cin >> docCliente;
+
+        float cantidadDisponible = Isla::getCantidadDisponible(tipoCombustible);
+
+        if (cantidadCombustible > cantidadDisponible) {
+            cantidadCombustible = cantidadDisponible;
+            cout << "En este momento no disponemos de esa cantidad de combustible. " << endl;
+            cout << "Se expedira y cobrara la cantidad disponible." << endl;
+        }
+
+        monto = cantidadCombustible * precioCombustible;
+
+        // Crear nueva transaccion
+        Transaccion* nuevaTransaccion = new Transaccion(tipoCombustible, cantidadCombustible, formaPago,
+                                                        docCliente, monto, precioCombustible);
+        // Registrar la transacción
+        transacciones_[numTransacciones_] = nuevaTransaccion;
+
+        // Incrementar el contador
+        numTransacciones_++;
+
+        cout << "Venta realizada con exito." << endl;
+
+        return;
+
+    } else {
+        cout << "El surtidor no esta activo." << endl;
+    }
+}
+
+void PuntoSurtidor::registrarVenta(Transaccion* transaccion) {
+    // Aumentar el tamaño del arreglo dinamico si es necesario
+    if (this->numTransacciones_ == 10) {
+        // Crear un nuevo arreglo de punteros a Transaccion con mayor tamaño
+        Transaccion** temp = new Transaccion*[20];
+
+        for (unsigned int i = 0; i < this->numTransacciones_; i++) {
+            temp[i] = this->transacciones_[i]; // Copiar puntero, no objeto
+        }
+
+        // Liberar el arreglo antiguo
+        delete[] this->transacciones_;
+        this->transacciones_ = temp; // Asignar el nuevo arreglo
+    }
+
+    // Registrar la nueva transacción
+    this->transacciones_[this->numTransacciones_] = transaccion; // Asignar el puntero de la nueva transacción
+    this->numTransacciones_++; // Incrementar el contador de transacciones
+}
+
+void PuntoSurtidor::mostrarHistorico() {
+    cout << "Historial de transacciones:\n";
+
+    if (numTransacciones_ == 0) {
+        cout << "No hay transacciones registradas." << endl;
+        return;
+    }
+
+    for (unsigned int i = 0; i < numTransacciones_; ++i) {
+        transacciones_[i]->mostrarTransaccion(); //Imprimir el recibo de la transaccion
+    }
+}
+
+PuntoSurtidor::~PuntoSurtidor() {
+    for (unsigned int i = 0; i < numTransacciones_; ++i) {
+        delete transacciones_[i];
+    }
+    delete[] transacciones_;
+}
