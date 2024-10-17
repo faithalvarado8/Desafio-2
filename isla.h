9abb54f2e04ffa92c248026499a8c38981fd90af
CarlaZapata14 <carla.zapata@udea.edu.co>
2024-10-16 23:44:43 -0500

    Update isla.h

--- isla.h
+++ isla.h
@@ -17,18 +17,17 @@ public:
     Isla(string nombre, unsigned int capacidadMaxima = 2);
     ~Isla();
 
-    PuntoSurtidor* getPuntoSurtidor(unsigned int indice);  // Método para obtener un surtidor por índice
+    PuntoSurtidor* getPuntoSurtidor(unsigned int indice);  // Metodo para obtener un surtidor por indice
     unsigned int getNumSurtidores() const;
 
     string getNombreIsla();
+
     void listarPuntosSurtidores();
-    void agregarPuntoSurtidor(PuntoSurtidor* nuevoSurtidor);  // Método para agregar surtidores
-    // void eliminarPuntoSurtidor(unsigned int indice);
+    void agregarPuntoSurtidor(PuntoSurtidor* nuevoSurtidor);  // Metodo para agregar surtidores
     void eliminarPuntoSurtidor(const string& codigo);
 
-    // // Método estático temporal que devuelve una cantidad de combustible disponible para pruebas
+    // // Metodo estatico temporal que devuelve una cantidad de combustible disponible para pruebas
     static float getCantidadDisponible(const string& tipoCombustible);
 };
 
 #endif // ISLA_H
-
